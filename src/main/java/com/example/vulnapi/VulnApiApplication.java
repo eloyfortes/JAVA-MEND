c81a13e873370bae0f599491e9e4f8aa30fdb866
Eloy <fortesgdaniel@gmail.com>
2026-01-03 21:29:36 -0300

    init

---
+++ src/main/java/com/example/vulnapi/VulnApiApplication.java
@@ -0,0 +1,11 @@
+package com.example.vulnapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VulnApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VulnApiApplication.class, args);
+    }
+}
